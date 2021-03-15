4ab20b79dfff5d20f617212fd9412010ca4de2ce
Vova188292 <78851876+Vova188292@users.noreply.github.com>
2021-03-15 08:41:12 +0300

    burds

---
+++ птицы/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace птицы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kivi a = new Kivi();
+            Duck b = new Duck();
+            Penguin c = new Penguin();
+            Budgerigar d = new Budgerigar();
+            a.move();
+            b.move();
+            c.move();
+            d.move();
+        }
+    }
+}
---
+++ птицы/birds.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace птицы
+{
+    interface Ibird
+    {
+       string Fluing();
+       string Swim();
+        
+    }
+    abstract class Birds:Ibird
+    {
+        public string Fluing()
+        {
+            return ("Птица летит");
+        }
+        public string Swim()
+        {
+           return ("Птица плывет");
+        }
+    }
+    class Kivi :Birds
+    {
+       public void move()
+        {
+            Console.WriteLine("Птица класса киви умеет:" + Swim());
+        }
+    }
+    class Duck:Birds
+    {
+        public void move()
+        {
+            Console.WriteLine("Птица класса утка умеет:" + Swim() + " " + Fluing());
+        }
+    }
+    class Penguin : Birds
+    {
+        public void move()
+        {
+            Console.WriteLine("Птица класса пенгвин умеет:" + Swim());
+        }
+    }
+    class Budgerigar : Birds
+    {
+        public void move()
+        {
+            Console.WriteLine("Птица класса волнистый попугай умеет:" + Swim());
+        }
+    }
+}
